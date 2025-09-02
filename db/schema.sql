1d0e5e33ddaa06ad4f401d377353cff6fed6c2b5
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-01 22:33:29 -0500

    created tables and content for tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,33 @@
--- TODO
+DROP TABLE IF EXISTS orders_products;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS users; 
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username text NOT NULL UNIQUE,
+  password text NOT NULL
+);
+
+CREATE TABLE orders (
+  id serial PRIMARY KEY,
+  date date NOT NULL,
+  note text,
+  user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE products (
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  description text NOT NULL,
+  price decimal NOT NULL
+);
+
+CREATE TABLE orders_products (
+  id serial PRIMARY KEY,
+  quantity int NOT NULL,
+
+  order_id int NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+  product_id int NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+  UNIQUE(order_id, product_id)
+);
