6f50eda80fde231c2e568805dfaadbf939a7abc9
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-02 20:41:13 -0500

    added await functions as well as a try catch to await for sql queries and reroll seed values to test for duplicates

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,8 @@
 import db from "#db/client";
 import { createProduct } from "#db/queries/products";
 import { faker } from "@faker-js/faker";
+import { createUser } from "#db/queries/users";
+import { createOrder, createOrderProduct } from "#db/queries/orders";
 
 await db.connect();
 await seed();
@@ -14,17 +16,17 @@ async function seed() {
       username: faker.internet.username(),
       password: faker.internet.password(),
     };
-    // createUser(user)
+    await createUser(user);
   }
 
   // Create 5 Orders for the users
   for (let i = 1; i <= 5; i++) {
     const order = {
-      date: faker.date.date(),
+      date: faker.date.anytime(),
       note: faker.lorem.sentence(),
-      user_id: Math.ceil(Math.random() * 5),
+      userId: Math.ceil(Math.random() * 5),
     };
-    // createOrder(order)
+    await createOrder(order);
   }
 
   // Create 15 products
@@ -34,16 +36,23 @@ async function seed() {
       price: faker.commerce.price(),
       description: faker.commerce.productDescription(),
     };
-    // createProduct(product);
+    await createProduct(product);
   }
 
   // Have those orders include 5 items
   for (let i = 1; i <= 15; i++) {
-    const orderProduct = {
-      order_id: Math.ceil(Math.random() * 5),
-      product_id: Math.ceil(Math.random() * 15),
-      quantity: Math.ceil(Math.random() * 5),
-    };
-    // createOrderProduct(orderProduct)
+    try {
+      const orderProduct = {
+        orderId: Math.ceil(Math.random() * 5),
+        productId: Math.ceil(Math.random() * 15),
+        quantity: Math.ceil(Math.random() * 5),
+      };
+      await createOrderProduct(orderProduct);
+    } catch (err) {
+      if (err.code == "23505") {
+        i--;
+        // console.error("Product order already exists. Rerolling...");
+      }
+    }
   }
 }
