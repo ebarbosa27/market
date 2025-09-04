644fb9e45052dc6ecb96e2318d0bc0a57bab2429
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-03 21:44:53 -0500

    developed orders api to include request to create and retrieve orders as well as products within those orders

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -1,16 +1,16 @@
 import db from "#db/client";
 
-export async function createOrder({ date, note, userId }) {
+export async function createOrder({ date, userId, note }) {
   const sql = `
   INSERT INTO orders 
-    (date, note, user_id)
+    (date, user_id, note)
   VALUES
     ($1, $2, $3)
   RETURNING *
   `;
   const {
     rows: [order],
-  } = await db.query(sql, [date, note, userId]);
+  } = await db.query(sql, [date, userId, note]);
   return order;
 }
 
@@ -26,4 +26,33 @@ export async function createOrderProduct({ orderId, productId, quantity }) {
     rows: [orderProduct],
   } = await db.query(sql, [orderId, productId, quantity]);
   return orderProduct;
+}
+
+export async function getOrderByUserId(userId) {
+  const sql = `SELECT * FROM orders WHERE user_id = $1`;
+  const { rows: orders } = await db.query(sql, [userId]);
+  return orders;
+}
+
+export async function getOrderById(id) {
+  const sql = `SELECT * FROM orders WHERE id = $1`;
+  const {
+    rows: [order],
+  } = await db.query(sql, [id]);
+  return order;
+}
+
+export async function getProductsByOrderId(orderId) {
+  const sql = `
+  SELECT 
+    products.* 
+  FROM 
+    orders 
+    JOIN orders_products ON orders_products.order_id = orders.id 
+    JOIN products ON products.id = orders_products.product_id
+  WHERE 
+    orders.id = $1
+  `;
+  const { rows: orders } = await db.query(sql, [orderId]);
+  return orders;
 }
